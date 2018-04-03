e50c57f08dd85c9885df4233906383220fb8121f
Heineek <ekg@ngs.ru>
2018-04-03 17:05:12 +0300

    03-04-2018

--- config.py
+++ config.py
@@ -14,4 +14,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['ekg@bastard']
+    POSTS_PER_PAGE = 3
 
