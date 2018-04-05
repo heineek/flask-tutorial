dcba5227c45b4db452e9640277cfc8fa594ae8af
Heineek <ekg@ngs.ru>
2018-04-05 13:52:58 +0300

    05-04-2018

--- app/routes.py
+++ app/routes.py
@@ -152,7 +152,7 @@ def explore():
                            next_url=next_url, prev_url=prev_url)
 
 
-@app.route('/reset_password_request', method=['GET', 'POST'])
+@app.route('/reset_password_request', methods=['GET', 'POST'])
 def reset_password_request():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
